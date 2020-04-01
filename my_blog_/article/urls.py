f965c478e6e7222258d4426e9d457a3dadf2366b
18280914502 <2605645264@qq.com>
2020-04-01 18:03:05 +0800

    update article

--- my_blog_/article/urls.py
+++ my_blog_/article/urls.py
@@ -7,16 +7,18 @@ app_name = 'article'
 urlpatterns = [
 
     path('article-list/', views.article_list, name='article_list'),
-# 文章详情
+    # 文章详情
     path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
     # 写文章
     path('article-create/', views.article_create, name='article_create'),
     # 删除文章
     path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
-# 安全删除文章
+    # 安全删除文章
     path(
         'article-safe-delete/<int:id>/',
         views.article_safe_delete,
         name='article_safe_delete'
     ),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
 ]
